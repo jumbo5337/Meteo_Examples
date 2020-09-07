c0427622a82819945760abaa0a482161bea0f211
Oleg Lobanov <o.lobanov@crpt.ru>
2020-09-07 15:34:48 +0300

    Shallow Water Model

--- simple_hydrodinamic_model/lib.py
+++ simple_hydrodinamic_model/lib.py
@@ -2,34 +2,32 @@ import numpy as np
 from numpy import sin, gradient
 
 
-dn = 100000
-dt = 30
 
 def coriolis_parameter (fi = 60):
     fi = np.radians(fi)  # Кассчёт на широте 60 градусов
     w = 7.29e-5  # Угловая скорость вращения земли
     return 2 * w * sin(fi)
 
-def load_file(filename, x_step = dn, y_step=dn):
+def load_file(filename, dn):
     F = np.loadtxt(filename, delimiter=' ')  # Загружаем данные по геопотенциалу
-    X = [i * x_step for i in range(len(F[0]))]  # Задаем массивы для координат
-    Y = [i * y_step for i in range(len(F))]
+    X = [i * dn for i in range(len(F[0]))]  # Задаем массивы для координат
+    Y = [i * dn for i in range(len(F))]
     return X,Y,F
 
-def calc_wind(F):
+def calc_wind(F, dn):
     l = coriolis_parameter()
     dy, dx = gradient(F, dn)  # Считаем градиенты геопотенциала по Oy и Ox
     ug = -dy * l  # Рассчитываем компоненты геострофического ветра
     vg = dx * l
     return ug,vg
 
-def predict_step(F,ug,vg, dt = dt):
+def predict_step(F,ug,vg, dt, dn):
     l = coriolis_parameter()
     Fdy,Fdx = gradient(F, dn)
     ugDy, ugDx = gradient(ug, dn)
     vgDy, vgDx = gradient(vg, dn)
-    ugP = ug - dt*(ug*ugDx + vg*ugDy + Fdx + l*vg)
-    vgP = vg - dt*(ug*vgDx + vg*vgDy + Fdy - l*ug)
+    ugP = ug - dt*(ug*ugDx + vg*ugDy) -dt*Fdx +dt* l*vg
+    vgP = vg - dt*(ug*vgDx + vg*vgDy) -dt*Fdy -dt*l*ug
     FuDx = gradient(F*ug, dn)[1]
     FvDy = gradient(F*vg, dn)[0]
     Fp = F - dt*(FuDx + FvDy)
--- simple_hydrodinamic_model/shallow_water_app.py
+++ simple_hydrodinamic_model/shallow_water_app.py
@@ -3,11 +3,11 @@ from simple_hydrodinamic_model.lib import load_file, calc_wind, coriolis_paramet
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
-plt.rcParams['animation.convert_path'] = r'C:\Program Files\ImageMagick-7.0.10-Q16\magick.exe'
+# plt.rcParams['animation.convert_path'] = r'C:\Program Files\ImageMagick-7.0.10-Q16\magick.exe'
 dn = 100000
 l = coriolis_parameter()
-X, Y, F = load_file(r'GEO.dat', dn, dn)
-ug, vg = calc_wind(F)
+X, Y, F = load_file(r'GEO.dat', dn)
+ug, vg = calc_wind(F, dn)
 steps = 3000
 dt = 300
 F_arr = np.zeros(shape=(steps, 40, 40), dtype=np.float64)
@@ -21,22 +21,24 @@ vg_arr[0] = vg
 ug_arr[0] = ug
 
 for i in range(1, steps):
-    Fp,ug,vg = predict_step(F_arr[i-1], ug_arr[i-1], vg_arr[i-1], 5)
+    Fp,ug,vg = predict_step(F_arr[i-1], ug_arr[i-1], vg_arr[i-1], 5, dn)
     F_arr[i] = Fp
     vg_arr[i] = vg
     ug_arr[i] = ug
-levels = []
-min = np.min(F[:][:])-500
-for i in range(30):
-    dn = (np.max(F[:][:]) - np.min(F[:][:]))/20
-    min += dn
-    levels.append(min)
+
+print(ug_arr[-1])
+print("=========")
+print(vg_arr[-1])
+
+
+levels = np.mgrid[50000:60000:20j]
 
 #Вывод результатов ветер
 fig, ax = plt.subplots(figsize=(8, 8))
-islolines = ax.contour(X, Y, F, levels=levels, colors='black')  # Рисуем изолинии
+isolines = ax.contour(X, Y, F, levels=levels, colors='black')  # Рисуем изолинии
+labels = ax.clabel(isolines,levels)
 gradient = ax.contourf(X, Y, F, levels=levels)  # Заливаем градиентом
-# bar = fig.colorbar(gradient)  # Выставляем градиентную шкалу
+bar = fig.colorbar(gradient)  # Выставляем градиентную шкалу
 # wind = ax.quiver(X, Y, ug, vg, scale = 10)  # Наносим векторы ветр
 ax.set_title('Поле геострофического ветра на уровне 500 гПа')
 ax.set_aspect('equal')
@@ -46,18 +48,22 @@ ax.set_aspect('equal')
 
 def animate(i):
     global X,Y,ax
-    # global wind
-    global islolines, gradient
+    global wind
+    global isolines, gradient, labels
     Fp = F_arr[i]
-    ugp,vgp = ug_arr[i], vg_arr[i]
+    # ugp,vgp = ug_arr[i], vg_arr[i]
     # wind.set_UVC(ugp, vgp)  # Наносим векторы ветр
-    [i.remove() for i in islolines.collections]
+    [i.remove() for i in isolines.collections]
     [i.remove() for i in gradient.collections]
-    islolines = ax.contour(X, Y, Fp, levels=levels, colors='black')  # Рисуем изолинии
-    gradient = ax.contourf(X, Y, Fp, levels=levels)  # Заливаем градиентом
+    isolines = ax.contour(X, Y, Fp, levels=levels, colors='black')  # Рисуем изолинии
+    gradient = ax.contourf(X, Y, Fp, levels=levels)  # Заливаем гради
+    for i in labels: i.remove()
+    ticks = {}
+    for i in levels :
+        ticks[i] = str(int(i/100))
+    labels = ax.clabel(isolines, levels, fmt= ticks, fontsize=8, inline=True)
     # wind.remove()
     # wind = ax.quiver(X, Y, ugp, vgp, scale=1, scale_units = 'inches')
 
 anim = FuncAnimation(fig, animate, frames= steps, interval=1, blit=False)
-anim.save('shallow_water.gif', writer='imagemagick')
 plt.show()
